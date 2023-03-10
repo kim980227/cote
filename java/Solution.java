a812cbdb01fa9972a3efb6cf757a6655561ddc36
kim980227 <sear227@gmail.com>
2023-03-10 19:46:47 +0900

    1948

--- java/Solution.java
+++ java/Solution.java
@@ -1,30 +1,33 @@
 import java.util.Scanner;
+
 public class Solution {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for(int t=1; t<=T; t++) {
-            int n = sc.nextInt();
-            String s = ""; // 리스트보다 스트링(char의 리스트형이라고 생각)
+        int t = sc.nextInt();
+        for (int test_case = 1; test_case < t + 1; test_case++) {
+            int[] src = new int[2];
+            int[] dest = new int[2];
+            int[] dom = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+            int answer = 0;
 
-            for(int i=0; i<n; i++) {
-                String ch = sc.next();
-                int r = sc.nextInt();
-
-                for (int j=0; j<r; j++){
-                    s += ch; // 스트링 컨캣(문자하니씩 반복수만큼 이어붙이기)
+            src[0] = sc.nextInt();
+            src[1] = sc.nextInt();
+            dest[0] = sc.nextInt();
+            dest[1] = sc.nextInt();
+            if (src[0] == dest[0])
+                answer = dest[1] - src[1] + 1;
+            else {
+                answer += dest[1]+1;
+                answer += (dom[src[0]-1]-src[1]);
+                while(src[0] < (dest[0]-1)){
+                    answer += dom[src[0]];
+                    src[0] +=1 ;
                 }
             }
 
-            System.out.printf("#%d\n", t);
+            System.out.printf("#%d %d\n", test_case, answer);
 
-            for(int i=0; i<s.length(); i++) { // 스트링 길이만큼 반복
-                System.out.print(s.charAt(i)); // 스트링 인덱스에 대응되는 문자 하니씩 출력
-                if( i%10 ==9 ) // 너비가 10 고정, 증가하는 i를 다시 0으로 초기화 필요 없이 10으로 나눈 나머지값으로 분기식 작성
-                    System.out.println();
-            }
-            System.out.println(); // 이거까지 있어야 모든 테케 통과 왜지?=>하나의 테스트케이스가 끝나면 줄바꿈을 해야줘야됨 안그러면 이전 출력 에 #test_case가 붙여줘서 나옴.
-//swea1946
-        }
+
+        }//test_case loop
     }
 }
