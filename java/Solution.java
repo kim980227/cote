0553d9863b181525072eb3ddd63bbd73990836ae
kim980227 <sear227@gmail.com>
2023-03-11 15:35:56 +0900

    binary search

--- java/Solution.java
+++ java/Solution.java
@@ -1,34 +1,33 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
+
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        for (int test_case = 1; test_case < t + 1; test_case++) {
-            int[] dx = {0, 1, 0, -1};
-            int[] dy = {1, 0, -1, 0};
+        int target = sc.nextInt(); // 찾고자하는 값
+        Random rand = new Random(10);
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            int n = rand.nextInt(10);
+            arr[i] = n;
+        }//배열 채워넣기
 
-            int n = sc.nextInt();
-            int[][] nums = new int[n][n];
+        int len = arr.length - 1;
+        int stt = 0;
+        for (int x : arr) {
+            System.out.printf("%d ", x);
+            int mid = (len + stt) / 2;
 
-            int dir = 0; // 현재 방향 인데스
-            int x = 0, y = 0;
-            for (int i = 1; i <= n * n; i++) {
-                nums[x][y]=i; //nums 채우기
-                if (x + dx[dir] >= n || x + dx[dir] < 0 || y + dy[dir] >= n || y + dy[dir] < 0 || nums[x + dx[dir]][y + dy[dir]] != 0) {
-                    //좌표가 n*n 를 벗어나지 않는 지 체크 + nums에 값이 입력되었는지 체크
-                    dir = (dir + 1) % 4; // 방향전환 (배열 크기로 나누면 dir를 0으로 초기화 하지 않아도 됨)
-                }
-                x += dx[dir];
-                y += dy[dir];
+            if(target == x){
+                System.out.printf("\n%d is here",x);
+                break;
             }
-            System.out.printf("#%d\n", test_case);
-            for(int i = 0; i<n; i++){
-                for (int j : nums[i]){
-                    System.out.printf("%d ",j);
-                }
-                System.out.println();
+            else if(target > x){
+                stt = mid +1;
             }
-        }//test_case loop
+            else if(x > target){
+                len = mid-1;
+            }
+        }
     }
 }
